68a0991035559352b1c1d4cc3db60546f66f17f0
AndreiAkimochkin <AndreiSverige@gmail.com>
2021-03-14 15:45:29 +0100

    5. Add token for auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,8 @@ const User = require("../models/users");
 const router = new Router();
 const bcrypt = require('bcryptjs');
 const {check, validationResult} = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 router.post('/registration',
     [
@@ -24,7 +26,7 @@ router.post('/registration',
         if ({candidate}) {
             return res.status(400).json({message: `User with email ${email} is already exist`})
         }
-        const hashPassword = await  bcrypt.hash(password, 15);
+        const hashPassword = await  bcrypt.hash(password, 8);
         const user = new User({email, password: hashPassword});
         await user.save();
         return res.json({message: 'User was created'})
@@ -33,7 +35,37 @@ router.post('/registration',
     catch (e) {
         res.send({message: "Server error"})
     }
-})
+});
+
+router.post('/login',
+    async (req, res)=> {
+        try {
+            const {email, password} = req.body
+            const user = await User.findOne({email})
+            if(!user) {
+                return res.status(400).json({message: "User not found"})
+            }
+            const isPassValid = bcrypt.compareSync(password, user.password)
+            if (!isPassValid) {
+                return res.status(400).json({message: "Invalid password"})
+            }
+            const jwt = jwt.sign({id: user.id}, config.get("secretKey"), {expiresIn: "3h"})
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    diskSpace: user.diskSpace,
+                    usedSpace: user.usedSpace,
+                    avatar: user.avatar
+                }
+            })
+        }
+
+        catch (e) {
+            res.send({message: "Server error"})
+        }
+    })
 
 module.exports =router;
 
